9a42f6be5b30456397b00ea75555efeec650d7ee
ltanh07 <luutuananh9bthcstm@gmail.com>
2023-12-06 21:16:02 +0700

    Nhac

---
+++ Music Player in JavaScript/Music Player in JavaScript/js/music-list.js
@@ -0,0 +1,24 @@
+// muốn thêm đoạn nhạc thì coppy rồi cho lên
+
+//   {
+//     name: "têm",
+//     artist: "tác giả ghi gì cũng dc",
+//     img: "tệp ảnh ở img ",
+//     src: "ơ tệp song"
+//   }
+
+let allMusic = [
+  {
+    name: "Last Christmas - Star remix",
+    artist: "",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Obito - Đánh Đổi ft. MCK",
+    artist: "deg by phuocperry",
+    img: "music-2",
+    src: "music-2"
+  },
+  
+];
